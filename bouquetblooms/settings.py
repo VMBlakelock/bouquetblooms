e53f4b122d8b943a08ea2f3ee9c5a6e9c12498c1
Vickie <Vickie.Blakelock@gmail.com>
2020-12-15 21:50:46 +0000

    tried to rectify error of The SECRET_KEY setting must not be empty

--- bouquetblooms/settings.py
+++ bouquetblooms/settings.py
@@ -8,10 +8,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
-"""
 
-from pathlib import Path
+"""
 import os
+from os import path
+from pathlib import Path
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', "")
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
