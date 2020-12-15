c26514b635bb116f32ba438db53c3a0c65189835
Vickie <Vickie.Blakelock@gmail.com>
2020-12-15 20:43:00 +0000

    added new secret key

--- bouquetblooms/settings.py
+++ bouquetblooms/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4+e)6pi$2#0ktg1g&oo-%%23igcrt)mxrox*t!@=d7%73^i=@*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
